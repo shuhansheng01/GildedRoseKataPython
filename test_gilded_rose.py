85d070a55cd75cda1e5bf6c45ebfb82edae9b6a2
Shuhan Sheng <sheng.sh@northeastern.edu>
2024-10-01 20:44:08 -0700

    Update tests for GildedRoseKataPython latest changes

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -3,6 +3,12 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
+    
+    def test_foo(self):
+        items = [Item("foo", 0, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("fixme", items[0].name)
 
     # Test 1: Aged Brie increases in quality over time
     def test_aged_brie_quality_increase(self):
